8612a8ffe75cbf44f97423b5257b78625cc814b1
hafeezrq <hafeezrq@yahoo.com>
2022-08-24 17:31:10 +1000

    Initial structure and introduce location service.

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const SeasonDisplay = () => {
+  return <div>SeasonDisplay</div>;
+};
+
+export default SeasonDisplay;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+
+const el = document.getElementById('root');
+const root = ReactDOM.createRoot(el);
 
 const App = () => {
-  return <div>Hi, there!</div>;
+  let longitude = 0;
+  window.navigator.geolocation.getCurrentPosition(
+    position => console.log(position.coords.longitude),
+    err => console.log(err)
+  );
+  return <div>Longitude: {longitude}</div>;
 };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+root.render(<App />);
