372e9f08048f568d7ce2a402c4277d0c3ac24a5e
hafeezrq <hafeezrq@yahoo.com>
2022-08-24 20:47:47 +1000

    Refactor Functional Component into Class Component

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom/client';
 
 const el = document.getElementById('root');
 const root = ReactDOM.createRoot(el);
 
-const App = () => {
-  let longitude = 0;
-  window.navigator.geolocation.getCurrentPosition(
-    position => console.log(position.coords.longitude),
-    err => console.log(err)
-  );
-  return <div>Longitude: {longitude}</div>;
-};
+class App extends Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position.coords.longitude),
+      err => console.log(err)
+    );
+    return <div>Longitude:</div>;
+  }
+}
 
 root.render(<App />);
